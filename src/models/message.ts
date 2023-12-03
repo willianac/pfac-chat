d156f3e6b2ddb267a354a9bd0c975ef778f3c5a4
Willian Almeida <willianac01@gmail.com>
2023-12-03 19:05:36 -0300

    fix: fixed double websocket connections open

--- src/models/message.ts
+++ src/models/message.ts
@@ -1,4 +1,5 @@
 export type Message = {
+	id: string
 	senderName: string
 	receiverName: string
 	text: string
--- src/pages/chat/with/[id].tsx
+++ src/pages/chat/with/[id].tsx
@@ -1,4 +1,5 @@
 import ChatLayout from "@/components/chat-layout";
+import { Message } from "@/models/message";
 import { User } from "@/models/user";
 import { useUserStore } from "@/store/useUserStore";
 import { Button, Input } from "@chakra-ui/react";
@@ -8,12 +9,16 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client"
 
 export async function getServerSideProps() {
-  const res = await axios.get<User[]>("http://localhost:5000/users");
-  const data = res.data;
-  return { props: { data } };
+  const chatsResponse = await axios.get<User[]>("http://localhost:5000/users");
+	const previousMessagesResponse = await axios.get<Message[]>("http://localhost:5000/messages")
+
+  const chats = chatsResponse.data;
+	const previousMessages = previousMessagesResponse.data
+  return { props: { chats, previousMessages } };
 }
 
-function Chat({ data }: { data: User[] }) {
+function Chat({ chats, previousMessages }: { chats: User[], previousMessages: Message[] }) {
+	const [messagesState, setMessagesState] = useState(previousMessages)
 	const [websocket, setWebSocket] = useState<any>();
 	const [message, setMessage] = useState("")
 	const router = useRouter()
@@ -22,29 +27,54 @@ function Chat({ data }: { data: User[] }) {
 	const { user } = useUserStore()
 
 	useEffect(() => {
-		const ws = io("ws://localhost:5000", {auth: { userid: user.id }})
-		setWebSocket(ws)
-
-		ws.on("message", (e: any) => {
-			console.log(e)
-		})
-
-		return () => {
-			ws.disconnect()
+		async function getChatMessages() {
+			const res = await axios.get<Message[]>(`http://localhost:5000/messages?senderId=${user.id}&receiverId=${receiverID}`)
+			setMessagesState([...res.data])
 		}
+		getChatMessages()
 
 	}, [receiverID, user.id])
 
-	const sendMessage = () => {
 
-		websocket.emit("message", message)
+	useEffect(() => {
+		const ws = io("ws://localhost:5000", {auth: { userid: user.id }})
+		setWebSocket(ws)
+
+	}, [receiverID])
+
+	useEffect(() => {
+		if(websocket) {
+			websocket.on("connect", () => {
+				console.log("conectou")
+			})
+	
+			websocket.on("message", (responseMessage: Message) => {
+				setMessagesState(prevState => [...prevState, responseMessage])
+			})
+		}
+
+		return () => {
+			if(websocket) {
+				console.log("disconectando")
+				websocket.disconnect()
+			}
+		}
+
+	}, [websocket])
+
+
+	const sendMessage = () => {
+		websocket.emit("message", {message, receiverId: receiverID})
 	}
 
 	return (
-		<ChatLayout userList={data}>
+		<ChatLayout userList={chats}>
 			<div className="flex flex-col h-full">
 				<div className="h-[94%]">
-					teste
+					{messagesState.map(mes => {
+						console.log(mes)
+						return <h1 key={mes.id} className="text-white">{mes.text}</h1>
+					})}
 				</div>
 				<div className="flex-1 flex gap-2">
 					<Input onChange={(e) => setMessage(e.target.value)} type="text" placeholder="Digite sua mensagem" size="lg" color="white"/>
