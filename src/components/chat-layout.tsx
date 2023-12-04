c66692101c4a03ffd7436a5bd6806417beb515c1
Willian Almeida <willianac01@gmail.com>
2023-12-04 15:40:24 -0300

    fix: added a function to remove the user itself from the chat list

--- src/components/chat-layout.tsx
+++ src/components/chat-layout.tsx
@@ -2,8 +2,9 @@ import { Box } from "@chakra-ui/react";
 import Image from "next/image";
 import default_profile_img from "../../public/default_user.jpg"
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { User } from "@/models/user";
+import { useUserStore } from "@/store/useUserStore";
 
 type LayoutProps = {
 	children: React.ReactNode,
@@ -12,6 +13,13 @@ type LayoutProps = {
 
 function ChatLayout({ children, userList }: LayoutProps) {
 	const router = useRouter()
+	const { user } = useUserStore()
+	const [chatList, setChatList] = useState<User[]>([])
+
+	useEffect(() => {
+		const removeItselfFromList = userList.filter(chatUser => chatUser.id !== user.id)
+		setChatList(removeItselfFromList)
+	}, [])
 
 	return (
 		<div className="w-full flex justify-center mt-24">
@@ -20,7 +28,7 @@ function ChatLayout({ children, userList }: LayoutProps) {
 					<h2 className="text-2xl font-semibold text-light">Conversar com:</h2>
 
 					<ul className="mt-9 flex flex-col gap-2">
-						{userList?.map((user) => (
+						{chatList?.map((user) => (
 							<li onClick={() => router.push(`/chat/with/${user.id}`, undefined, {shallow: true})} className="border-2 border-zinc-700 flex items-center gap-2.5 p-2.5 cursor-pointer hover:bg-zinc-700 rounded-xl" key={user.id}>
 								<Image src={user.profile_url ? user.profile_url : default_profile_img} width={30} height={30} alt="a default profile image" className="rounded-full"/>
 								<span className="text-light text-lg font-medium">{user.name}</span>
